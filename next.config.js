1f77eea704fe2ff132d1ebc067cd804e250b9265
Gabriel <gabrielmonteiroduete@gmail.com>
2023-10-01 23:10:23 -0300

    chore: separate files home

--- next.config.js
+++ next.config.js
@@ -2,14 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  pageExtensions: [
-    'page.tsx',
-    'page.ts',
-    'page.jsx',
-    'page.js',
-    'index.tsx',
-    'index.jsx',
-  ],
 }
 
 module.exports = nextConfig
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,15 @@
 import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt'
+import type { NextPage } from 'next'
 import { useEffect } from 'react'
 import { useGlitch } from 'react-powerglitch'
 import Sound from 'react-sound'
 
 import Equalizer from '../src/components/Equalizer'
+import * as S from '../src/styles/stylesHome'
 import { SoundClickButton, SoundGlitch } from '../src/utils/Sounds'
-import { settingsSound } from './settings'
-import * as S from './styles'
+import { settingsSound } from '../src/utils/settingsHome'
 
-const Home = () => {
+const Home: NextPage = () => {
   const glitch = useGlitch()
 
   useEffect(() => {
