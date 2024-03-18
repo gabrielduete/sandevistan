806a21dd91addfeebf8fb19a1a9702cff7b5e52a
Gabriel <gabrielmonteiroduete@gmail.com>
2024-03-17 23:55:59 -0300

    feat: configure path jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const nextJest = require('next/jest')
+const path = require('path')
 
 const createJestConfig = nextJest({
   dir: './',
@@ -9,8 +10,11 @@ const customJestConfig = {
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^~/src/enums/medias$': path.resolve(__dirname, './src/enums/medias'),
+  },
 }
 
-module.exports = createJestConfig(customJestConfig)
+module.exports = createJestConfig({ ...customJestConfig })
 
 /** o/ font: https://fek.io/blog/add-jest-testing-framework-to-an-existing-next-js-app */
