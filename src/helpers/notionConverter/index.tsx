8ab70b4887ff28e5e91b3b69a1bfd593862ea40a
Gabriel <gabrielmonteiroduete@gmail.com>
2024-06-11 00:23:38 -0300

    feat: adjust new tags notion converter

---
+++ src/helpers/notionConverter/index.tsx
@@ -0,0 +1,195 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import Link from 'next/link'
+import React, { Fragment, ReactNode } from 'react'
+import CopyText from '~/src/components/CopyText'
+import Text from '~/src/components/Text'
+
+import { Block, ListBlock } from './notionConverter.types'
+import * as S from './styles'
+
+export const renderNestedList = (blocks: ListBlock): ReactNode => {
+  const { children } = blocks
+  if (!children) return null
+
+  const isNumberedList = children[0]?.type === 'numbered_list_item'
+  return isNumberedList ? (
+    <ol>{children.map(renderBlock)}</ol>
+  ) : (
+    <ul>{children.map(renderBlock)}</ul>
+  )
+}
+
+export const renderBlock = (block: Block): ReactNode => {
+  const { type, id } = block
+  const value = block[type as keyof Block] as any
+  const text = value?.rich_text
+
+  switch (type) {
+    case 'paragraph':
+      return (
+        <S.Text key={id}>
+          <Text title={text} />
+        </S.Text>
+      )
+
+    case 'heading_1':
+      return (
+        <h1 key={id}>
+          <Text title={text} />
+        </h1>
+      )
+
+    case 'heading_2':
+      return (
+        <h2 key={id}>
+          <Text title={text} />
+        </h2>
+      )
+
+    case 'heading_3':
+      return (
+        <h3 key={id}>
+          <Text title={text} />
+        </h3>
+      )
+
+    case 'bulleted_list':
+      return (
+        <ul key={id}>{(value as ListBlock)?.children?.map(renderBlock)}</ul>
+      )
+
+    case 'numbered_list':
+      return (
+        <ol key={id}>{(value as ListBlock)?.children?.map(renderBlock)}</ol>
+      )
+
+    case 'bulleted_list_item':
+    case 'numbered_list_item':
+      return (
+        <li key={id}>
+          <Text title={value?.rich_text} />
+          {!!value?.children && renderNestedList(block as ListBlock)}
+        </li>
+      )
+
+    case 'to_do':
+      return (
+        <div key={id}>
+          <label htmlFor={id}>
+            <input type='checkbox' id={id} defaultChecked={value?.checked} />
+            <Text title={value?.rich_text} />
+          </label>
+        </div>
+      )
+
+    case 'toggle':
+      return (
+        <details key={id}>
+          <summary>
+            <Text title={value?.rich_text} />
+          </summary>
+          {block.children?.map(child => (
+            <Fragment key={child.id}>{renderBlock(child)}</Fragment>
+          ))}
+        </details>
+      )
+
+    case 'child_page':
+      return (
+        <div key={id}>
+          <strong>{value?.title}</strong>
+          {block.children?.map(renderBlock)}
+        </div>
+      )
+
+    case 'image':
+      const src =
+        value?.type === 'external' ? value.external?.url : value?.file?.url
+      const caption = value?.caption ? value.caption[0]?.plain_text : ''
+      console.log(src)
+      return (
+        <figure key={id}>
+          <img src={src} alt={caption} />
+          {caption && <figcaption>{caption}</figcaption>}
+        </figure>
+      )
+
+    case 'divider':
+      return <hr key={id} />
+
+    case 'quote':
+      return <blockquote key={id}>{value?.rich_text[0].plain_text}</blockquote>
+
+    case 'code':
+      const code = value?.rich_text[0].plain_text
+
+      return (
+        <S.WrapperCode>
+          <S.WrapperIcons>
+            Code
+            <CopyText value={code} />
+          </S.WrapperIcons>
+          <pre key={id}>
+            <var>{code}</var>
+          </pre>
+        </S.WrapperCode>
+      )
+
+    case 'file':
+      const srcFile =
+        value?.type === 'external' ? value.external?.url : value?.file?.url
+      const fileName = srcFile?.split('/').pop()?.split('?')[0]
+      const fileCaption = value?.caption ? value.caption[0]?.plain_text : ''
+
+      return (
+        <figure key={id}>
+          <div>
+            📎
+            <Link href={srcFile} passHref>
+              {fileName}
+            </Link>
+          </div>
+          {fileCaption && <figcaption>{fileCaption}</figcaption>}
+        </figure>
+      )
+
+    case 'bookmark':
+      return (
+        <a key={id} href={value?.url} target='_blank' rel='noreferrer noopener'>
+          {value?.url}
+        </a>
+      )
+
+    case 'table':
+      return (
+        <table key={id}>
+          <tbody>
+            {block.children?.map((child, index) => {
+              const rowValue = child.table_row
+              const RowElement =
+                value?.has_column_header && index === 0 ? 'th' : 'td'
+
+              return (
+                <tr key={child.id}>
+                  {rowValue?.cells.map((cell, i) => (
+                    <RowElement key={`${cell.plain_text}-${i}`}>
+                      <Text title={cell} />
+                    </RowElement>
+                  ))}
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      )
+
+    case 'column_list':
+      return <div key={id}>{block.children?.map(renderBlock)}</div>
+
+    case 'column':
+      return <div key={id}>{block.children?.map(renderBlock)}</div>
+
+    default:
+      return 'Unsupported tag'
+  }
+}
---
+++ src/helpers/notionConverter/notionConverter.types.ts
@@ -0,0 +1,170 @@
+type TextContent = {
+  content: string
+}
+
+type RichText = {
+  text: TextContent
+  plain_text: string
+}
+
+type BlockBase = {
+  id: string
+  type: string
+  children?: Block[]
+}
+
+type ParagraphBlock = BlockBase & {
+  type: 'paragraph'
+  paragraph: {
+    rich_text: TextContent[]
+  }
+}
+
+type Heading1Block = BlockBase & {
+  type: 'heading_1'
+  heading_1: {
+    rich_text: TextContent[]
+  }
+}
+
+type Heading2Block = BlockBase & {
+  type: 'heading_2'
+  heading_2: {
+    rich_text: TextContent[]
+  }
+}
+
+type Heading3Block = BlockBase & {
+  type: 'heading_3'
+  heading_3: {
+    rich_text: TextContent[]
+  }
+}
+
+type BulletedListBlock = ListBlock & {
+  type: 'bulleted_list'
+}
+
+type NumberedListBlock = ListBlock & {
+  type: 'numbered_list'
+}
+
+type ListItemBlock = BlockBase & {
+  type: 'bulleted_list_item' | 'numbered_list_item'
+  rich_text: TextContent[]
+}
+
+type ToDoBlock = BlockBase & {
+  type: 'to_do'
+  to_do: {
+    rich_text: TextContent[]
+    checked: boolean
+  }
+}
+
+type ToggleBlock = BlockBase & {
+  type: 'toggle'
+  toggle: {
+    rich_text: TextContent[]
+  }
+}
+
+type ChildPageBlock = BlockBase & {
+  type: 'child_page'
+  child_page: {
+    title: string
+  }
+}
+
+type ImageBlock = BlockBase & {
+  type: 'image'
+  image: {
+    type: 'external' | 'file'
+    external?: { url: string }
+    file?: { url: string }
+    caption?: TextContent[]
+  }
+}
+
+type DividerBlock = BlockBase & {
+  type: 'divider'
+}
+
+type QuoteBlock = BlockBase & {
+  type: 'quote'
+  quote: {
+    rich_text: TextContent[]
+  }
+}
+
+type CodeBlock = BlockBase & {
+  type: 'code'
+  code: {
+    rich_text: TextContent[]
+  }
+}
+
+type FileBlock = BlockBase & {
+  type: 'file'
+  file: {
+    type: 'external' | 'file'
+    external?: { url: string }
+    file?: { url: string }
+    caption?: TextContent[]
+  }
+}
+
+type BookmarkBlock = BlockBase & {
+  type: 'bookmark'
+  bookmark: {
+    url: string
+  }
+}
+
+type TableBlock = BlockBase & {
+  type: 'table'
+  table: {
+    has_column_header: boolean
+  }
+}
+
+type ColumnListBlock = BlockBase & {
+  type: 'column_list'
+}
+
+type ColumnBlock = BlockBase & {
+  type: 'column'
+}
+
+export type TableRowBlock = BlockBase & {
+  type: 'table_row'
+  table_row: {
+    cells: RichText[][]
+  }
+}
+
+export type ListBlock = BlockBase & {
+  children: Block[]
+}
+
+export type Block =
+  | ParagraphBlock
+  | Heading1Block
+  | Heading2Block
+  | Heading3Block
+  | BulletedListBlock
+  | NumberedListBlock
+  | ListItemBlock
+  | ToDoBlock
+  | ToggleBlock
+  | ChildPageBlock
+  | ImageBlock
+  | DividerBlock
+  | QuoteBlock
+  | CodeBlock
+  | FileBlock
+  | BookmarkBlock
+  | TableBlock
+  | TableRowBlock
+  | ColumnListBlock
+  | ColumnBlock
